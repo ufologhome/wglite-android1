f27c4dcdb10422a774220d1e42bdda1cf5321f72
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-26 21:38:52 +0300

    Implement start and stop VPN service functions
    Added functions to start and stop VPN service on Android.

--- vpnservice.py
+++ vpnservice.py
@@ -1,1 +1,26 @@
+from jnius import autoclass
+from android import activity
+
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+VpnService = autoclass('android.net.VpnService')
+Intent = autoclass('android.content.Intent')
+
+def start_vpn():
+    ctx = PythonActivity.mActivity
+    intent = VpnService.prepare(ctx)
+    if intent:
+        ctx.startActivityForResult(intent, 0)
+    else:
+        _start_service()
+
+def _start_service():
+    ctx = PythonActivity.mActivity
+    intent = Intent(ctx, autoclass('org.kivy.android.PythonService'))
+    intent.putExtra("serviceClass", "vpnservice.MyVpnService")
+    ctx.startService(intent)
+
+def stop_vpn():
+    ctx = PythonActivity.mActivity
+    intent = Intent(ctx, autoclass('org.kivy.android.PythonService'))
+    ctx.stopService(intent)
 
